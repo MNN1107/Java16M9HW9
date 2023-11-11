f4d1c84ac173872cb92eeb20a7f5503460ee4136
MNN1107 <144456106+MNN1107@users.noreply.github.com>
2023-11-11 23:57:48 +0200

    Update MyArrayListTest.java
    додані перевірки в рядках 17, 21, 22

--- Java16M9HW9/src/MyArrayListTest.java
+++ Java16M9HW9/src/MyArrayListTest.java
@@ -14,9 +14,12 @@ public class MyArrayListTest {
         System.out.println("arrayList.size() = " + arrayList.size());
 
         System.out.println("arrayList.get(0)= " + arrayList.get(0));
+        System.out.println("arrayList.get(-1)= " + arrayList.get(-1));
 
         arrayList.remove(2);
         System.out.println("arrayList.remove(2)= " + arrayList);
+        arrayList.remove(8);
+        System.out.println("arrayList.remove(8)= " + arrayList);
 
         arrayList.clear();
         System.out.println("arrayList.clear()= " + arrayList);
